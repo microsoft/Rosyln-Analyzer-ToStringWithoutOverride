5f6b49a0a205ec527753f264c5332473a00ec53f
Dan Stiner <dastiner@microsoft.com>
2015-12-21 11:49:43 -0800

    Mech: Unused using

--- ImplicitStringConversionAnalyzer/ImplicitStringConversionAnalyzer.Test/UnitTests.cs
+++ ImplicitStringConversionAnalyzer/ImplicitStringConversionAnalyzer.Test/UnitTests.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestHelper;
