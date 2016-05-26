010bbb32e4dae0655e9be3698673b8c2df08f50f
Dan Stiner <dastiner@microsoft.com>
2016-05-26 16:54:57 -0700

    Rename main project

--- ImplicitStringConversionAnalyzer/Roslyn-Analyzer-ToStringWithoutOverride/Resources.Designer.cs
+++ ImplicitStringConversionAnalyzer/Roslyn-Analyzer-ToStringWithoutOverride/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ImplicitStringConversionAnalyzer {
+namespace Rosyln.Analyzer.ToStringWithOverride {
     using System;
     using System.Reflection;
     
@@ -40,7 +40,7 @@ namespace ImplicitStringConversionAnalyzer {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ImplicitStringConversionAnalyzer.Resources", typeof(Resources).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Rosyln.Analyzer.ToStringWithOverride.Resources", typeof(Resources).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
