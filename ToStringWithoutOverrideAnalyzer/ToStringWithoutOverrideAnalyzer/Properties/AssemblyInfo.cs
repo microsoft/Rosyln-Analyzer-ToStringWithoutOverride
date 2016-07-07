47c47657473b18adf7c25b55438179d034d1301c
Dan Stiner <dastiner@microsoft.com>
2016-07-06 23:45:45 -0700

    Reset to build settings used by default Roslyn analyzer template

--- ToStringWithoutOverrideAnalyzer/ToStringWithoutOverrideAnalyzer/Properties/AssemblyInfo.cs
+++ ToStringWithoutOverrideAnalyzer/ToStringWithoutOverrideAnalyzer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("ToStringWithoutOverrideAnalyzer")]
-[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyCopyright("Copyright Microsoft © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
